08ad82bd8d0cef98f921d60fe17952d35d77fac7
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-22 22:22:32 +0300

    controller

---
+++ PersonelWebApi/Controllers/PersonelsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonelWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PersonelsController : ControllerBase
+    {
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return Create();
+        }
+    }
+}
